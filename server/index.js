4215e05f4418a1d9c966398a6d5b537f749ae13c
iamkaviyarasan <kaviyarasangunasekaran@gmail.com>
2024-03-23 10:51:15 +0530

    BUG FIXED

--- server/index.js
+++ server/index.js
@@ -9,14 +9,13 @@ app.use (express.json())
 
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-app.post('/add', (req, res  =>{
-    const task =req.body.task;
-    todomodel.create({
-        task:task
-    }).then(result =>res.json(result))
-    .catch(err => res.json(err))
+app.post('/add',(req,res)   =>{
+    const task=req.body.task;
+    TodoModel.create({task:task})
+    .then(result=>res.json(result))
+    .catch(err=>res.json(err))
 
-}))
+})git 
 app.listen(3001, () =>{
     console.log("Server is Running")
 })
